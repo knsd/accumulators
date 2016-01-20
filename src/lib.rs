7173519c471839c6dc6b37ec80710578efb41737
Fedor Gogolev <knsd@knsd.net>
2016-01-20 22:08:35 +0300

    Add SummNone accumulator

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,22 @@ impl Accumulator for Summ {
     }
 }
 
+struct SummNone {
+    inner: Option<f64>,
+}
+
+impl Accumulator for SummNone {
+    type Input = f64;
+    type Output = Option<f64>;
+
+    fn notify(&mut self, value: Self::Input) {
+        self.inner = match self.inner {
+            None => Some(value),
+            Some(inner) => Some(inner + value),
+        }
+    }
+
+    fn result(&self) -> Self::Output {
+        self.inner
+    }
+}
