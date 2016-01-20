fddf7b9dacfee89525f9171e888507d2c98fa6b6
Fedor Gogolev <knsd@knsd.net>
2016-01-20 22:05:18 +0300

    Implement Summ accumulator

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,21 @@ pub trait Accumulator {
     fn notify(&mut self, value: &Self::Input) -> ();
     fn result(&self) -> Self::Output;
 }
+
+struct Summ {
+    inner: f64,
+}
+
+impl Accumulator for Summ {
+    type Input = f64;
+    type Output = f64;
+
+    fn notify(&mut self, value: &Self::Input) {
+        self.inner = self.inner + value
+    }
+
+    fn result(&self) -> Self::Output {
+        self.inner
+    }
 }
 
