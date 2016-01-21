61c6bad3ccc6c017d55db2f1427e731574bbc20d
Fedor Gogolev <knsd@knsd.net>
2016-01-21 12:27:40 +0300

    Return Accumulator trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-pub trait Add<Input> {
-    fn add(&mut self, value: Input) -> ();
+pub trait Accumulator {
+    type Input;
+    type Output;
+
+    fn add(&mut self, value: Self::Input) -> ();
+    fn result(&self) -> Self::Output;
 }
 
 // Numeric
@@ -8,9 +12,16 @@ struct Summ {
     inner: f64,
 }
 
-impl Add<f64> for Summ {
-    fn add(&mut self, value: f64) {
+impl Accumulator for Summ {
+    type Input = f64;
+    type Output = f64;
+
+    fn add(&mut self, value: Self::Input) {
         self.inner = self.inner + value
+    }
+
+    fn result(&self) -> Self::Output {
+        self.inner
     }
 }
 
@@ -18,12 +29,19 @@ struct SummNone {
     inner: Option<f64>,
 }
 
-impl Add<f64> for SummNone {
-    fn add(&mut self, value: f64) {
+impl Accumulator for SummNone {
+    type Input = f64;
+    type Output = Option<f64>;
+
+    fn add(&mut self, value: Self::Input) {
         self.inner = match self.inner {
             None => Some(value),
             Some(inner) => Some(inner + value),
         }
+    }
+
+    fn result(&self) -> Self::Output {
+        self.inner
     }
 }
 
@@ -31,9 +49,16 @@ struct Last {
     inner: Option<f64>,
 }
 
-impl Add<f64> for Last {
-    fn add(&mut self, value: f64) {
+impl Accumulator for Last {
+    type Input = f64;
+    type Output = Option<f64>;
+
+    fn add(&mut self, value: Self::Input) {
         self.inner = Some(value)
+    }
+
+    fn result(&self) -> Self::Output {
+        self.inner
     }
 }
 
@@ -41,9 +66,11 @@ struct Min {
     inner: Option<f64>,
 }
 
-impl Add<f64> for Min {
+impl Accumulator for Min {
+    type Input = f64;
+    type Output = Option<f64>;
 
-    fn add(&mut self, value: f64) {
+    fn add(&mut self, value: Self::Input) {
         match self.inner {
             None => self.inner = Some(value),
             Some(inner) => if inner > value {
@@ -51,21 +78,31 @@ impl Add<f64> for Min {
             },
         }
     }
+
+    fn result(&self) -> Self::Output {
+        self.inner
+    }
 }
 
 struct Max {
     inner: Option<f64>,
 }
 
-impl Add<f64> for Max {
+impl Accumulator for Max {
+    type Input = f64;
+    type Output = Option<f64>;
 
-    fn add(&mut self, value: f64) {
+    fn add(&mut self, value: Self::Input) {
         match self.inner {
             None => self.inner = Some(value),
             Some(inner) => if inner < value {
                 self.inner = Some(value)
             },
         }
+    }
+
+    fn result(&self) -> Self::Output {
+        self.inner
     }
 }
 
@@ -75,14 +112,19 @@ struct ListAvg {
     inner: Vec<f64>
 }
 
-impl Add<f64> for ListAvg {
-    fn add(&mut self, value: f64) {
+impl Accumulator for ListAvg {
+    type Input = f64;
+    type Output = Option<f64>;
+
+    fn add(&mut self, value: Self::Input) {
         self.inner.push(value)
     }
-}
 
-impl<'a> Add<&'a [f64]> for ListAvg {
-    fn add(&mut self, value: &'a [f64]) {
-        self.inner.extend_from_slice(value)
+    fn result(&self) -> Self::Output {
+        if self.inner.is_empty() {
+            return None
+        } else {
+            return Some(self.inner.iter().fold(0.0, |a, b| a + b) / self.inner.len() as f64)
+        }
     }
 }
