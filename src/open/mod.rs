090a101ac27bb153f9e1bb98d7bb6351a53618d7
Fedor Gogolev <knsd@knsd.net>
2016-01-24 02:37:44 +0300

    Add container type

--- src/open/mod.rs
+++ src/open/mod.rs
@@ -88,3 +88,35 @@ impl Accumulator for Max {
         }
     }
 }
+
+type AccContainer<A> = HashMap<String, A>;
+
+struct Container {
+    summ: AccContainer<Summ>,
+    summ_none: AccContainer<SummNone>,
+    last: AccContainer<Last>,
+    min: AccContainer<Min>,
+    max: AccContainer<Max>,
+}
+
+trait ContainerNotify<Acc> {
+    fn notify(&mut self, name: &str, value: f64);
+}
+
+macro_rules! make_notify {
+    ($acc: ty, $field: ident) => {
+        impl ContainerNotify<$acc> for Container {
+
+            fn notify(&mut self, name: &str, value: f64) {
+                let acc = self.$field.entry(name.to_string()).or_insert_with(|| Accumulator::new() );
+                acc.add(value)
+            }
+        }
+    }
+}
+
+make_notify!(Summ, summ);
+make_notify!(SummNone, summ_none);
+make_notify!(Last, last);
+make_notify!(Min, min);
+make_notify!(Max, max);
