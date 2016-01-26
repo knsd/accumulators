08bc6bffafca00a859b10d12a066ff6d4a6ce6e6
Fedor Gogolev <knsd@knsd.net>
2016-01-26 06:03:32 +0300

    Add fnv hasher to close

--- src/close/mod.rs
+++ src/close/mod.rs
@@ -1,4 +1,37 @@
 use std::collections::{HashMap};
+use std::collections::hash_state::DefaultState;
+use std::default::Default;
+use std::hash::Hasher;
+
+pub struct FnvHasher(u64);
+
+impl Default for FnvHasher {
+
+    #[inline]
+    fn default() -> FnvHasher {
+        FnvHasher(0xcbf29ce484222325)
+    }
+}
+
+impl Hasher for FnvHasher {
+
+    #[inline]
+    fn finish(&self) -> u64 {
+        self.0
+    }
+
+    #[inline]
+    fn write(&mut self, bytes: &[u8]) {
+        let FnvHasher(mut hash) = *self;
+
+        for byte in bytes.iter() {
+            hash = hash ^ (*byte as u64);
+            hash = hash.wrapping_mul(0x100000001b3);
+        }
+
+        *self = FnvHasher(hash);
+    }
+}
 
 enum Accumulator {
     Summ(f64),
